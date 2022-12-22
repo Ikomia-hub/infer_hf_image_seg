e45209e0838851d6ca32a5378877d65c65ef2a89
Allan <allan.kouidri@gmail.com>
2022-12-22 10:25:08 +0100

    Define output using CInstanceSegIO

--- infer_huggingface_image_segmentation_process.py
+++ infer_huggingface_image_segmentation_process.py
@@ -28,7 +28,6 @@ import torch
 import random
 import io
 from PIL import Image
-import cv2
 import os
 
 
@@ -44,7 +43,6 @@ class InferHuggingfaceImageSegmentationParam(core.CWorkflowTaskParam):
         #self.cuda = torch.cuda.is_available()
         self.cuda = True if torch.cuda.is_available() else False
         self.model_name = "facebook/detr-resnet-50-panoptic"
-        self.model_card = "facebook/detr-resnet-50-panoptic"
         self.checkpoint_path = ""
         self.checkpoint = False
         self.conf_thres = 0.5
@@ -82,10 +80,7 @@ class InferHuggingfaceImageSegmentation(dataprocess.C2dImageTask):
     def __init__(self, name, param):
         dataprocess.C2dImageTask.__init__(self, name)
         # Add input/output of the process here
-        self.setOutputDataType(core.IODataType.IMAGE_LABEL, 0)
-        self.addOutput(dataprocess.CImageIO())
-        self.addOutput(dataprocess.CNumericIO())
-        self.addOutput(dataprocess.CGraphicsOutput())
+        self.addOutput(dataprocess.CInstanceSegIO())
 
         # Create parameters class
         if param is None:
@@ -109,66 +104,12 @@ class InferHuggingfaceImageSegmentation(dataprocess.C2dImageTask):
         # This is handled by the main progress bar of Ikomia application
         return 1
 
-    def infer(self, img, graphics_output):
-
-        param = self.getParam()
-
-        # Image pre-pocessing (image transformation and conversion to PyTorch tensor)
-        encoding  = self.feature_extractor(img, return_tensors="pt")
-        if param.cuda is True:
-            #encoding = encoding.cuda()
-            encoding = encoding.to(self.device)
-        # Prediction
-        outputs = self.model(**encoding)
-
-        processed_sizes = torch.as_tensor(encoding['pixel_values'].shape[-2:]).unsqueeze(0)
-
-        result = self.feature_extractor.post_process_panoptic(
-                                                            outputs,
-                                                            processed_sizes,
-                                                            threshold=param.conf_thres
-                                                            )[0]
-
-
-        # Segments info and the panoptic result from DETR's prediction
-        segments_info = deepcopy(result["segments_info"])
-
-        # Panoptic predictions are stored in a special format png
-        masks = Image.open(io.BytesIO(result['png_string']))
-  
-        # We convert the png into an segment id map
-        h, w, c = np.shape(img)
-        masks = np.array(masks, dtype=np.uint8)
-        masks = cv2.resize(masks, (w,h), interpolation = cv2.INTER_NEAREST)
-        masks = torch.from_numpy(rgb_to_id(masks))
-
-        panoptic_seg = torch.full((h, w), fill_value=0)
-
-        # Convertion of the class id
-        for i in range(len(segments_info)):
-            c = segments_info[i]["category_id"]
-            segments_info[i]["category_id"] = self.meta.thing_dataset_id_to_contiguous_id[c]\
-            if segments_info[i]["isthing"] else self.meta.stuff_dataset_id_to_contiguous_id[c]
-
-        for info in segments_info:
-            offset = len(self.thing_classes) if not info["isthing"] else 0
-            px_value = info["id"]
-            cat_value = info["category_id"]
-            bool_mask = masks == px_value
-            y, x = np.median(bool_mask.cpu().numpy().nonzero(), axis=1)
-            properties_text = core.GraphicsTextProperty()
-            properties_text.color = self.colors[cat_value+offset]
-            properties_text.font_size = 7
-            graphics_output.addText(self.classes[offset + cat_value], x, y, properties_text)
-            panoptic_seg[bool_mask] = cat_value+1+offset
-        return panoptic_seg.cpu().numpy()
-
     def run(self):
         # Core function of your process
         # Call beginTaskRun for initialization
         self.beginTaskRun()
 
-        self.forwardInputImage(0, 1)
+        self.forwardInputImage(0, 0)
 
         param = self.getParam()
 
@@ -198,33 +139,114 @@ class InferHuggingfaceImageSegmentation(dataprocess.C2dImageTask):
             # Setting color
             np.random.seed(10)
             self.colors = np.array(np.random.randint(0, 255, (len(self.classes), 3)))
-            self.colors = [[0,0,0]]+[[int(c[0]), int(c[1]), int(c[2])] for c in self.colors]
-            self.setOutputColorMap(1, 0, self.colors)
-     
-            # Setting legend
-            legend = self.getOutput(2)
-            legend.addValueList(list(range(1,1+len(self.classes))), "Class value", self.classes)
+            self.colors = [[int(c[0]), int(c[1]), int(c[2])] for c in self.colors]
+            self.setOutputColorMap(0, 1, self.colors)
 
             param.update = False
 
         #Get input
         input = self.getInput(0)
-
-        # Get output
-        graphics_output = self.getOutput(3)
-        graphics_output.setImageIndex(1)
-        graphics_output.setNewLayer("PanopticSegmentation")
-        if input.isDataAvailable():
-            img = input.getImage()
-            output_mask = self.getOutput(0)
-            out = self.infer(img, graphics_output)
-            output_mask.setImage(out)
+        image = input.getImage()
+        self.infer(image)
 
         # Step progress bar:
         self.emitStepProgress()
 
         # Call endTaskRun to finalize process
         self.endTaskRun()
+
+    def infer(self, image):
+
+        param = self.getParam()
+
+        # Image pre-pocessing (image transformation and conversion to PyTorch tensor)
+        encoding  = self.feature_extractor(image, return_tensors="pt")
+        if param.cuda is True:
+            encoding = encoding.to(self.device)
+        # Prediction
+        outputs = self.model(**encoding)
+        processed_sizes = torch.as_tensor(encoding['pixel_values'].shape[-2:]).unsqueeze(0)
+
+        h, w, c = np.shape(image)
+        result = self.feature_extractor.post_process_panoptic(
+                                                            outputs,
+                                                            processed_sizes,
+                                                            threshold=param.conf_thres,
+                                                            target_sizes=[(h,w)]
+                                                            )[0]
+
+        result_with_score = self.feature_extractor.post_process_instance_segmentation(
+                                                            outputs,
+                                                            threshold=param.conf_thres,
+                                                            target_sizes=[(h, w)],
+                                                            return_coco_annotation=True,
+                                                            )[0]["segments_info"]
+        # Get score                                        
+        scores = [r["score"] for r in result_with_score]
+
+        # Segments info and the panoptic result from DETR's prediction
+        segments_info = deepcopy(result["segments_info"])
+
+        # Get output :
+        h, w, _ = np.shape(image)
+        instance_output = self.getOutput(1)
+        instance_output.init("PanopticSegmentation", 0, w, h)
+
+        # Panoptic predictions are stored in a special format png
+        masks = Image.open(io.BytesIO(result['png_string']))
+
+        # We convert the png into an segment id map
+        masks = np.array(masks, dtype=np.uint8)
+        mask_id = rgb_to_id(masks)
+        unique_colors = np.unique(mask_id).tolist()
+
+        masks_binary = np.zeros((h,w))
+        mask_list = []
+        for color in unique_colors:
+            object_mask = np.where(mask_id == color, 1, 0)
+            mask_list.append(object_mask)
+            masks_binary = np.dstack([object_mask, masks_binary])
+
+        # Get bounding boxes from masks
+        boxes = []
+        for i in range(masks_binary.shape[-1]):
+            m = masks_binary[:, :, i]
+            # Bounding box.
+            horizontal_indicies = np.where(np.any(m, axis=0))[0]
+            vertical_indicies = np.where(np.any(m, axis=1))[0]
+            if horizontal_indicies.shape[0]:
+                x1, x2 = horizontal_indicies[[0, -1]]
+                y1, y2 = vertical_indicies[[0, -1]]
+            boxes.append([x1, y1, x2, y2])
+        boxes = boxes[:-1]
+        boxes.reverse()
+
+        # Convertion of the class id
+        for i in range(len(segments_info)):
+            c = segments_info[i]["category_id"]
+            segments_info[i]["category_id"] = self.meta.thing_dataset_id_to_contiguous_id[c]\
+            if segments_info[i]["isthing"] else self.meta.stuff_dataset_id_to_contiguous_id[c]
+
+        for info,b, s, ml in zip(segments_info, boxes, scores, mask_list):
+            x_obj = float(b[0])
+            y_obj = float(b[1])
+            h_obj = (float(b[3]) - y_obj)
+            w_obj = (float(b[2]) - x_obj)
+            offset = len(self.thing_classes) if not info["isthing"] else 0
+            thing_or_stuff = 0 if info["isthing"] else 1
+            cat_value = info["category_id"]
+            instance_output.addInstance(
+                                    info["id"],
+                                    thing_or_stuff,
+                                    info["category_id"],
+                                    self.classes[offset + cat_value],
+                                    s,
+                                    x_obj,
+                                    y_obj,
+                                    w_obj,
+                                    h_obj,
+                                    ml,
+                                    self.colors[cat_value+offset])
 
 
 # --------------------
