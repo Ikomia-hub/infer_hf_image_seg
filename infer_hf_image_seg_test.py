2dfab91905d99c0eff13f7b0da6500e1d8e43500
ambroise <ambroise.berthe@ikomia.com>
2023-08-03 15:28:13 +0200

    Fix test file

--- infer_hf_image_seg_test.py
+++ infer_hf_image_seg_test.py
@@ -1,6 +1,5 @@
 import logging
 import cv2
-from ikomia.core import task
 from ikomia.utils.tests import run_for_test
 
 
@@ -9,10 +8,10 @@ logger = logging.getLogger(__name__)
 
 def test(t, data_dict):
     logger.info("===== Test::infer Hugging Face panoptic segmentation=====")
-    params = task.get_parameters(t)
+    params = t.get_param_object()
     params["model_name"] = "facebook/detr-resnet-50-panoptic"
     img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
     input_img_0 = t.get_input(0)
     input_img_0.set_image(img)
-    task.set_parameters(t, params)
+    t.set_parameters(params)
     yield run_for_test(t)
